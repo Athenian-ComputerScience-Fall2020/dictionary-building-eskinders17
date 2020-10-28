1b3faf8d45575d2dcbe709ae9f374cf1f51d8422
eskinders17 <22ehaileyesus@athenian.org>
2020-10-28 21:55:35 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,17 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
+continent_and_country = {}
+
+while True:
+    continent = input("Enter a continent name: ")
+    country =  input("Enter a country name: ")
+    continent_and_country[continent] = country
+    print("Do you want to put annother entry?")
+    x = input("Enter yes or no: ")
+    if x == 'yes':
+        continue 
+    else:
+        break
+for key, value in continent_and_country.items():
+    print(key, value)
